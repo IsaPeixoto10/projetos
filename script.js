33505aa6ed19cfa8ff6987281929531778835594
IsaPeixoto10 <isabelle.peixoto6@aluno.senai.br>
2025-05-14 08:43:01 -0300

    primeiroGIT

---
+++ script.js
@@ -0,0 +1,11 @@
+function boasVindas() {
+    alert('Bem-vindo ao site Saúde Mental em Foco!');
+}
+
+function agradecerResposta() {
+    alert(`Agradecemos a sua resposta enviada`);
+}
+
+function salvarResposta() {
+    alert(`Usuário cadastrado!`)
+}
